79b91f875c5b6c130d7e2ad508f5576ae4e7ca53
yoon <yunkh2005@naver.com>
2022-09-24 21:46:48 +0900

    2022.09.23

---
+++ src/num1008.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class num1008 {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        double a, b;
+        a = userInput.nextDouble();
+        b = userInput.nextDouble();
+        System.out.println(a / b);
+    }
+}
